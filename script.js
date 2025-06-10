8426759d57264bd6f4d804fe62b0213af1b8b56a
RomanRomaan <jay4or@gmail.com>
2025-06-10 16:44:20 +0300

    final

---
+++ script.js
@@ -0,0 +1,5 @@
+let firstRow = prompt("Enter first row")
+let secondRow = prompt("Enter second row")
+let thirdRow = prompt("Enter third row")
+
+alert(`Your first row: ${firstRow}, Your second row: ${secondRow}, Your third row: ${thirdRow}`)
