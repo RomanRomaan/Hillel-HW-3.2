6c5fb0f09d26adf0c99290b4ec7466c797b934e2
RomanRomaan <jay4or@gmail.com>
2025-06-11 16:55:33 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-let firstRow = prompt("Enter first row")
-let secondRow = prompt("Enter second row")
-let thirdRow = prompt("Enter third row")
+const firstRow = prompt("Enter first row")
+const secondRow = prompt("Enter second row")
+const thirdRow = prompt("Enter third row")
 
-alert(`Your first row: ${firstRow}, Your second row: ${secondRow}, Your third row: ${thirdRow}`)
+if ((firstRow === "" || secondRow === "" || thirdRow === "")) {
+    alert("Some of your row's are empty")
+} else if (firstRow.trim() === "" || secondRow.trim() === "" || thirdRow.trim() === "") {
+    alert("Some of your row's have only spaces")
+} else {
+    alert(`Your first row: ${firstRow}, Your second row: ${secondRow}, Your third row: ${thirdRow}`)
+}
